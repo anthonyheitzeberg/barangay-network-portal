9d248d2e4ce18cb636e93184678f3acd25d1755d
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2024-10-18 13:23:13 -0500

    feat: add primevue and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,49 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="card grid grid-cols-1 md:grid-cols-2 gap-4">
+    <InputGroup>
+      <InputGroupAddon>
+        <i class="pi pi-user"></i>
+      </InputGroupAddon>
+      <InputText v-model="text1" placeholder="Username" />
+    </InputGroup>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <InputGroup>
+      <InputGroupAddon>$</InputGroupAddon>
+      <InputNumber v-model="number" placeholder="Price" />
+      <InputGroupAddon>.00</InputGroupAddon>
+    </InputGroup>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <InputGroup>
+      <InputGroupAddon>www</InputGroupAddon>
+      <InputText v-model="text2" placeholder="Website" />
+    </InputGroup>
 
-  <RouterView />
+    <InputGroup>
+      <InputGroupAddon>
+        <i class="pi pi-map"></i>
+      </InputGroupAddon>
+      <Select
+        v-model="selectedCity"
+        :options="cities"
+        optionLabel="name"
+        placeholder="City"
+      />
+    </InputGroup>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import { ref } from 'vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+const text1 = ref(null)
+const text2 = ref(null)
+const number = ref(null)
+const selectedCity = ref()
+const cities = ref([
+  { name: 'New York', code: 'NY' },
+  { name: 'Rome', code: 'RM' },
+  { name: 'London', code: 'LDN' },
+  { name: 'Istanbul', code: 'IST' },
+  { name: 'Paris', code: 'PRS' },
+])
+</script>
